ddb32c279c4a8a3454bf00addb3e7916e718d686
Sveta <svetik.2007@list.ru>
2022-01-16 10:51:24 +0300

    zadanie_clear

--- beer_randomizer/uploader.js
+++ beer_randomizer/uploader.js
@@ -1,10 +1,3 @@
-/*function fdataRender(currentSelector, currentDataset, currentInfoName) {
-    let elemRender = document.querySelector(`${currentSelector} .data`);
-    elemRender.insertAdjacentText("beforeend", currentDataset[currentInfoName]);
-    elemRender.classList.remove("loading");
-}
-*/
-
 function fdataRender(currentSelector, currentDataset, currentInfoName) {
     let elemRender = document.querySelector(`${currentSelector} .data`);
     elemRender.innerHTML = currentDataset[currentInfoName];
@@ -15,9 +8,6 @@ function beerRandom() {
     fetch("https://random-data-api.com/api/beer/random_beer")
         .then((resp) => resp.json())
         .then(function (beerData) {
-            /*let shpora = document.querySelector(".cheating");
-            shpora.insertAdjacentText("beforeend", JSON.stringify(beerData));*/
-
             fdataRender(".brand", beerData, "brand");
 
             fdataRender(".name", beerData, "name");
@@ -43,10 +33,6 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch("https://random-data-api.com/api/users/random_user")
         .then((resp) => resp.json())
         .then(function (profileData) {
-
-            /*let shpora = document.querySelector(".cheating");
-            shpora.insertAdjacentText("beforeend", JSON.stringify(profileData));*/
-
             fdataRender(".first_name", profileData, "first_name");
 
             fdataRender(".last_name", profileData, "last_name");
